23bf250e73f7f26a2e6bf49f32a9825ec6eda868
simoes <596d642eedaf6449d9b5cbd74c150dccb64f689d>
2020-11-21 15:08:45 -0300

    feat: construido funcao de geracao de uuid

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-obterLentes();
-obterConfiguracaoFirebase()
+//obterLentes();
+//obterConfiguracaoFirebase()
+
+console.log(gerarUUID ());
 
 function obterConfiguracaoFirebase() {
 	var firebaseConfig = {
@@ -17,15 +19,32 @@ function obterConfiguracaoFirebase() {
 	var bancoDeDados = firebase.database();
 }
 
-function obterLentes() {
-	bancoDeDados.ref("lentes")	
-}
-
-function gravarLente(lente) {
-	bancoDeDados.ref("lentes")	
-}
+//function obterLentes() {
+//	bancoDeDados.ref("lentes")	
+//}
+//
+//function gravarLente(lente) {
+//	bancoDeDados.ref("lentes")	
+//}
 
 
 function gerarUUID () {
+	let x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'];	
+	let M = [1, 2, 3, 4, 5];
+	let N = [8, 9, 'a', 'b'];
 
+	return [[ x[obterNumeroRandomico(0, x.length)], x[obterNumeroRandomico(0, x.length)], x[obterNumeroRandomico(0, x.length)], x[obterNumeroRandomico(0, x.length)], x[obterNumeroRandomico(0, x.length)], x[obterNumeroRandomico(0, x.length)], x[obterNumeroRandomico(0, x.length)], x[obterNumeroRandomico(0, x.length)] ].join(""), 
+			[ x[obterNumeroRandomico(0, x.length)], x[obterNumeroRandomico(0, x.length)], x[obterNumeroRandomico(0, x.length)], x[obterNumeroRandomico(0, x.length)] ].join(""), 
+			[ M[obterNumeroRandomico(0, M.length)], x[obterNumeroRandomico(0, x.length)], x[obterNumeroRandomico(0, x.length)], x[obterNumeroRandomico(0, x.length)] ].join(""), 
+			[ N[obterNumeroRandomico(0, N.length)], x[obterNumeroRandomico(0, x.length)], x[obterNumeroRandomico(0, x.length)], x[obterNumeroRandomico(0, x.length)] ].join(""), 
+			[ x[obterNumeroRandomico(0, x.length)], x[obterNumeroRandomico(0, x.length)], x[obterNumeroRandomico(0, x.length)], x[obterNumeroRandomico(0, x.length)], x[obterNumeroRandomico(0, x.length)], x[obterNumeroRandomico(0, x.length)], x[obterNumeroRandomico(0, x.length)], x[obterNumeroRandomico(0, x.length)], x[obterNumeroRandomico(0, x.length)], x[obterNumeroRandomico(0, x.length)], x[obterNumeroRandomico(0, x.length)], x[obterNumeroRandomico(0, x.length)] ].join(""), ]
+			.join("-");		
+}
+
+
+
+function obterNumeroRandomico(min, max) {
+	min = Math.ceil(min); 
+	max = Math.floor(max - 1);
+	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
