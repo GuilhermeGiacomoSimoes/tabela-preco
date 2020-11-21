7e4b3ac3cb445c2c4d26dfe1553afebe8b02c48b
simoes <596d642eedaf6449d9b5cbd74c150dccb64f689d>
2020-11-21 18:05:28 -0300

    construido tabela de lentes

--- index.js
+++ index.js
@@ -26,41 +26,27 @@ function obterLentes() {
 	try {
 		var listaDeLentes = database.ref("lentes").orderByChild('descricao');	
 		listaDeLentes.on('value', snapshot => {
-			JSONParaArray(snapshot);	
+			construirTabelaDeLentes(snapshot.val());
 		});
 	}catch (exception) {
 		console.log("deu ruim: " + exception);
 	}
 }
 
-function JSONParaArray(json) {
-	let JSONString 			= JSON.stringify(json.val());
-	let arrayDeStringJSONs 	= JSONString.split("},");
-	let arrayDeJSONs 		= [];
-
-	for (let i=0; i<arrayDeStringJSONs.length; i ++) {
-		let string = arrayDeStringJSONs[i]; 
-
-		if (i==0) {
-			string = string + "}}";
-		}
-		else if (i==arrayDeStringJSONs.length - 1) {
-			string = "{" + string;
-		}
-		else {
-			string = "{" + string + "}}";
-		}
-
-		let json = JSON.parse(string);	
-		arrayDeJSONs.push(json);
-	} 
-
-	construirTabelaDeLentes(arrayDeJSONs);
-}
-
-
 function construirTabelaDeLentes(array){
+	for (let key in array){
+			let lente = array[key];
+			const html = `<span class="description"style="margin-left: 10%; width: 18%"> ${lente['descricao']}</span>
+						<span class="description" style="margin-left: 4%; width: 2%"> ${lente['empresa']} </span>
+						<span class="description"style="margin-left: 8%; width: 2%"> ${lente['tipo']} </span>
+						<span class="description"style="margin-left: 8%; width: 5%"> ${lente['custo']} </span>
+						<span class="description"style="margin-left: 6%; width: 4%"> ${lente['venda']} </span>
+						<button class="danger" style="margin-left: 8%;" onclick="deleteClient(${lente['uuid']})">Excluir</button>
+						<button class="positive" style="margin-left: 1%" onclick="editClient(${lente['uuid']})">Editar</button>
+						<hr style="margin-left: 10%; margin-right: 10%; opacity: 0.5">`;
 
+			document.getElementById('container_clients').innerHTML += html;
+	}
 }
 
 function gravarLente(lente) {
