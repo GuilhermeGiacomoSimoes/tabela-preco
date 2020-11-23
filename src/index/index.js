577dbfd48fd1e8a9a6af377ca5d9b0a3d4dabf3b
Guilherme <trintaeoitogc@gmail.com>
2020-11-23 20:28:27 -0300

    fix: try catch no deletaLente

--- src/index/index.js
+++ src/index/index.js
@@ -16,8 +16,9 @@ function deletaLente( uuid ) {
 	try {
 		let lenteRef = this.database.ref("lentes/" + uuid);
 		lenteRef.remove();
+
 		location.reload();
-	}catch(exception){
+	}catch (exception) {
 		console.log("deu ruim: " + exception);
 	}
 	
@@ -31,16 +32,15 @@ function construirTabelaDeLentes(array){
 	for (let key in array){
 			let lente = array[key];
 			let vista = lente['preco'] * lente['multiplicador'];
-			let uuid = ''+lente['uuid'];
 
 			const html = `<span class="description"style="margin-left: 10%; width: 15%"> ${lente['descricao']}</span>
-						  <span class="description" style="margin-left: 4%; width: 5%"> ${lente['empresa']} </span>
-						  <sp<span class="description" style="margin-left: 8%; width: 3%"> ${lente['tipo']} </span>
-						  <sp<span class="description" style="margin-left: 8%; width: 5%"> ${lente['preco']} </span>
-						  <sp<span class="description" style="margin-left: 6%; width: 4%"> ${vista} </span>
-						  <sp<button class="danger" style="margin-left: 8%;" onclick="deletaLente(\'${uuid}\')">Excluir</button>
-						  <sp<button class="positive" style="margin-left: 1%" onclick="editClient(\'${uuid}\')">Editar</button>
-						  <sp<hr style="margin-left: 10%; margin-right: 10%; opacity: 0.5">`;
+						<span class="description" style="margin-left: 4%; width: 5%"> ${lente['empresa']} </span>
+						<span class="description" style="margin-left: 8%; width: 3%"> ${lente['tipo']} </span>
+						<span class="description" style="margin-left: 8%; width: 5%"> ${lente['preco']} </span>
+						<span class="description" style="margin-left: 6%; width: 4%"> ${vista} </span>
+						<button class="danger" style="margin-left: 8%;" onclick="deletaLente(\'${lente['uuid']}\')">Excluir</button>
+						<button class="positive" style="margin-left: 1%" onclick="editClient(\'${lente['uuid']}\')">Editar</button>
+						<hr style="margin-left: 10%; margin-right: 10%; opacity: 0.5">`;
 
 			document.getElementById('container_lentes').innerHTML += html;
 	}
