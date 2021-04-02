89080a3be5e6658d955fdbd727bf51decdca77e7
Guilherme <trintaeoitogc@gmail.com>
2021-04-02 14:56:17 -0300

    fix: arrumado o boostrap na lista

--- src/index/index.js
+++ src/index/index.js
@@ -76,25 +76,24 @@ function construirTabelaDeLentes(array){
 	
 		const html = `
 		<div style="background-color: ${background_line}">
-			<span class="description"style="margin-left: 10%; width: 15%"> ${lente['descricao']}</span>
-			<span class="description" style="margin-left: 4%; width: 2%"> ${lente['empresa']} </span>
-			<span class="description" style="margin-left: 8%; width: 2%"> ${lente['tipo']} </span>
-			<span class="description" style="margin-left: 8%; width: 4%; color: ${color}"; > R$ ${lente['preco']} </span>
-			<span class="description" style="margin-left: 6%; width: 5%; color: ${color}"; > R$ ${lente['venda']} </span>
-			<button class="danger" style="margin-left: 5%; width: 120px; height: 50px;" onclick="confirmarDelecaoLente(\'${lente['uuid']}\')">
+			<span class="description col-xs-2  style="margin-left: 10%"> ${lente['descricao']}</span>
+			<span class="description col-xs-2" > ${lente['empresa']} </span>
+			<span class="description col-xs-2" > ${lente['tipo']} </span>
+			<span class="description col-xs-1" style="color: ${color}"; > R$ ${lente['preco']} </span>
+			<span class="description col-xs-1" style="color: ${color}"; > R$ ${lente['venda']} </span>
+			<button class="btn btn-danger col-xs-1" style="margin: 1%" onclick="confirmarDelecaoLente(\'${lente['uuid']}\')">
 				<center>
 					<img src="../../resources/trash.png" style="width: 20px; height: 20px;"/> 
 					Excluir
 				</center>
 			</button>
-			<button class="positive" style="margin-left: 1%; width: 120px; height: 50px;" onclick="editClient(\'${lente['uuid']}\')">
+			<button class="btn btn-primary col-xs-1" style="margin: 1%" onclick="editClient(\'${lente['uuid']}\')">
 				<center>
 					<img src="../../resources/edit.png" style="width: 20px; height: 20px;"/> 
 					Editar
 				</center>
 			</button>
-		<div>
-			`;
+		<div>`;
 
 		document.getElementById('container_lentes').innerHTML += html;
 	}
