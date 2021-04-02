50b167c07896489ec2f6eaeff7685329340abff8
Guilherme <trintaeoitogc@gmail.com>
2021-04-02 13:20:17 -0300

    feat: adicionado cores diferentes nas linhas e tirado espaço desnecessário

--- src/index/index.js
+++ src/index/index.js
@@ -53,38 +53,48 @@ function editClient( uuid ) {
 
 function construirTabelaDeLentes(array){
 	document.getElementById('container_lentes').innerHTML = "";
-
+	let index = 0;
+	
 	for (let key in array){
 		let lente       = array[key];
 		let promocional = lente['promocao'];
 		let color       = '#808080'; 
 		let preco       = lente['preco'];
+		let background_line = "#d3d3d3";
+		index ++;	
 
 		if (promocional) {
 			color = "#FF0000";
 			preco = lente['precoPromocional']; 
 		}
+
+		if ( index % 2 == 0 ) {
+			background_line = "#ffffff";
+		}
 		
 		let venda = preco * lente['multiplicador'];
 	
-		const html = `<span class="description"style="margin-left: 10%; width: 15%"> ${lente['descricao']}</span>
-				<span class="description" style="margin-left: 4%; width: 2%"> ${lente['empresa']} </span>
-				<span class="description" style="margin-left: 8%; width: 2%"> ${lente['tipo']} </span>
-				<span class="description" style="margin-left: 8%; width: 4%; color: ${color}"; > R$ ${lente['preco']} </span>
-				<span class="description" style="margin-left: 6%; width: 5%; color: ${color}"; > R$ ${lente['venda']} </span>
-				<button class="danger" style="margin-left: 5%; width: 120px; height: 50px;" onclick="confirmarDelecaoLente(\'${lente['uuid']}\')">
-					<center>
-						<img src="../../resources/trash.png" style="width: 20px; height: 20px;"/> 
-						Excluir
-					</center>
-				</button>
-				<button class="positive" style="margin-left: 1%; width: 120px; height: 50px;" onclick="editClient(\'${lente['uuid']}\')">
-					<center>
-						<img src="../../resources/edit.png" style="width: 20px; height: 20px;"/> 
-						Editar
-					</center>
-				</button>
-				<hr style="margin-left: 10%; margin-right: 20%; opacity: 0.5">`;
+		const html = `
+		<div style="background-color: ${background_line}">
+			<span class="description"style="margin-left: 10%; width: 15%"> ${lente['descricao']}</span>
+			<span class="description" style="margin-left: 4%; width: 2%"> ${lente['empresa']} </span>
+			<span class="description" style="margin-left: 8%; width: 2%"> ${lente['tipo']} </span>
+			<span class="description" style="margin-left: 8%; width: 4%; color: ${color}"; > R$ ${lente['preco']} </span>
+			<span class="description" style="margin-left: 6%; width: 5%; color: ${color}"; > R$ ${lente['venda']} </span>
+			<button class="danger" style="margin-left: 5%; width: 120px; height: 50px;" onclick="confirmarDelecaoLente(\'${lente['uuid']}\')">
+				<center>
+					<img src="../../resources/trash.png" style="width: 20px; height: 20px;"/> 
+					Excluir
+				</center>
+			</button>
+			<button class="positive" style="margin-left: 1%; width: 120px; height: 50px;" onclick="editClient(\'${lente['uuid']}\')">
+				<center>
+					<img src="../../resources/edit.png" style="width: 20px; height: 20px;"/> 
+					Editar
+				</center>
+			</button>
+		<div>
+			`;
 
 		document.getElementById('container_lentes').innerHTML += html;
 	}
