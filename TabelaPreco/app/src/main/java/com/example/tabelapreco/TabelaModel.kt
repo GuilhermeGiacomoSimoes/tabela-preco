20eae32edb003ace504663015361d3fb3d46deda
Guilherme <trintaeoitogc@gmail.com>
2021-04-30 22:05:07 -0300

    feat:  criado a Tabela Model

---
+++ TabelaPreco/app/src/main/java/com/example/tabelapreco/TabelaModel.kt
@@ -0,0 +1,17 @@
+package com.example.tabelapreco
+
+class TabelaModel {
+    lateinit var cilindrico: String
+    lateinit var descricao: String
+    lateinit var empresa: String
+    lateinit var esferico: String
+    lateinit var tipo: String
+    lateinit var uuid: String
+
+    var venda = .0
+    var diametro = 0
+    var indice = .0
+    var multplicadpr = 0
+    var preco = .0
+    var promocao = false
+}
