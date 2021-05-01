9f0c519abf8288efc3cbfb6fca8cb9c12391ea12
Guilherme <trintaeoitogc@gmail.com>
2021-04-30 21:58:22 -0300

    feat: inicio da implementação do repository

--- TabelaPreco/app/src/main/java/com/example/tabelapreco/TabelaRepository.kt
+++ TabelaPreco/app/src/main/java/com/example/tabelapreco/TabelaRepository.kt
@@ -1,4 +1,19 @@
 package com.example.tabelapreco
 
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
 class TabelaRepository {
+    private val database = Firebase.database;
+    val myRef = database.reference;
+
+    fun obterTabela(): Any? {
+        myRef.child("lentes").get().addOnSuccessListener {
+            return@addOnSuccessListener it.value as Unit
+        }.addOnFailureListener {
+
+        }
+
+        return null
+    }
 }
