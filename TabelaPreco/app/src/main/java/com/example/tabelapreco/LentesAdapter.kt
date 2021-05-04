cd63f198ff3d61fc24d4bf54cbe7607ee5eb4229
Guilherme <trintaeoitogc@gmail.com>
2021-05-03 22:38:04 -0300

    fet: criado um LentesAdapter pra lista de lentes

--- TabelaPreco/app/src/main/java/com/example/tabelapreco/LentesAdapter.kt
+++ TabelaPreco/app/src/main/java/com/example/tabelapreco/LentesAdapter.kt
@@ -1,4 +1,41 @@
 package com.example.tabelapreco
 
-class LentesAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+import java.util.ArrayList
+
+class LentesAdapter(private val list: ArrayList<Lente>, private val context: Context): BaseAdapter() {
+    override fun getCount() = list.size
+
+    override fun getItem(position: Int) = list[position]
+
+    override fun getItemId(position: Int): Long {
+        TODO("Not yet implemented")
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val layout = if( convertView == null ) {
+            val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            inflater.inflate(R.layout.lentes_adapter, null)
+        } else {
+            convertView
+        }
+
+        if(position % 2 == 0) {
+            layout.setBackgroundColor(0xf3f6f6)
+        }
+
+        val descricao = layout.findViewById<TextView>(R.id.descricao)
+        val preco = layout.findViewById<TextView>(R.id.preco)
+
+        descricao.text = list[position].descricao
+        preco.text = list[position].preco.toString()
+
+        return layout
+    }
+
 }
