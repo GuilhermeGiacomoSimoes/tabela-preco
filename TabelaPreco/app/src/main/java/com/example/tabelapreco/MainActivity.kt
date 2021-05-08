0c6b41e1dd821237b480bdaa95946614d9456b41
Guilherme <trintaeoitogc@gmail.com>
2021-05-08 11:40:09 -0300

    feat: criado a engenharia de pesquisa na mainActivity

--- TabelaPreco/app/src/main/java/com/example/tabelapreco/MainActivity.kt
+++ TabelaPreco/app/src/main/java/com/example/tabelapreco/MainActivity.kt
@@ -3,15 +3,19 @@ package com.example.tabelapreco
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.WindowManager
+import android.widget.EditText
 import android.widget.ListView
 import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
+import androidx.core.widget.addTextChangedListener
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
-import java.util.ArrayList
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,11 +32,32 @@ class MainActivity : AppCompatActivity() {
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
         window.statusBarColor = ContextCompat.getColor(this, R.color.main)
 
+        val edtPesquisa = findViewById<EditText>(R.id.edt_pesquisa)
+
+        edtPesquisa.addTextChangedListener( object  : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                val listaFiltrada = listaLente.filter {
+                    it.descricao.contains(s.toString().toUpperCase())
+                }
+
+                val l = ArrayList<Lente>()
+                l.addAll(listaFiltrada)
+                organizarLista(l)
+            }
+        })
+
         obterTodasLentes()
     }
 
     private fun organizarLista(list: ArrayList<Lente>) {
-        listaLente = list
         val tabela = findViewById<ListView>(R.id.tabela)
         tabela.adapter = LentesAdapter(list, this)
     }
