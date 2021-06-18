ce807bbc7ffb0511581f4b7b97a3319899f610d1
Guilherme Giácomo Simões <simoes@Latitude-E7450-8c8ac6a2.localdomain>
2021-06-18 00:04:19 -0300

    feat: adicionado arquivo para  monitoramento

---
+++ web/index.js
@@ -0,0 +1,28 @@
+var database = obterConfiguracaoFirebase();
+
+//verificarLogin('index');
+
+function logar() {
+	const email = document.getElementById("txtLogin").value;
+	const senha = document.getElementById("txtSenha").value;
+
+	try {
+		firebase.auth().signInWithEmailAndPassword(email, senha)
+		  .then((userCredential) => {
+			// Signed in
+				console.log(userCredential.refreshToken)
+			  	localStorage.setItem('token_tabela_preco', userCredential.refreshToken); 
+				verificarLogin('index');
+			// ...
+		  })
+		  .catch((error) => {
+			var errorCode = error.code;
+			var errorMessage = error.message;
+
+			  console.log(errorCode + ": " + errorMessage);
+		  });
+	}catch (exception) {
+		console.log("deu ruim: " + exception);
+	}
+}
+
